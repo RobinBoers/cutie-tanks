309d0ad31736634bfa6bda5bd7572bc7fafaf918
Robin Boers <robindev2019@outlook.com>
2021-07-27 09:40:21 +0200

    Added index.js for electron version

---
+++ index.js
@@ -0,0 +1,23 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+
+function createWindow () {
+  const mainWindow = new BrowserWindow({
+    width: 1320,
+    height: 990
+  })
+
+  mainWindow.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', function () {
+  app.quit()
+})
